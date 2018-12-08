488b34db8e152f31f3f779999430d6ca17a27145
Fabio Spampinato <spampinabio@gmail.com>
2018-12-08 03:37:33 +0100

    Properly importing `unstated-debug`

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import connect from 'unstated-connect2';
 import autosuspend from 'unstated-suspense-autosuspend';
 import {Container as BaseContainer} from 'unstated-suspense-middleware';
 import HMR from 'unstated-hmr';
-import debug from 'unstated-debug';
+import * as debug from 'unstated-debug';
 
 /* OVERSTATED */
 
