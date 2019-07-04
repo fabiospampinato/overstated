05d582fcacd11102c485431af48035d1f6b5b30c
Fabio Spampinato <spampinabio@gmail.com>
2019-07-04 23:11:10 +0200

    useStore: ensuring the selector is run also when subscribing to the store

--- src/use_store.ts
+++ src/use_store.ts
@@ -50,6 +50,8 @@ function useStore<S extends StoreType, R> ( store: S | Constructor<S>, selector:
 
     }
 
+    update ();
+
     instance.subscribe ( update );
 
     return () => instance.unsubscribe ( update );
